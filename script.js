3663c7364300ec6469b44fdc082ed59a656fb44d
jean pedro <jean.pedrooc@icloud.com>
2023-04-06 22:58:11 -0300

    Feature:edições

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn-mobile')
-
-function toglemenu () {
-    const nav = document.getElementById('nav')
-    nav.classList.toggle('active')
-}
-
-
-btn.addEventListener('click',toglemenu)
